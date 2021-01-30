fa855a175af77882358ef8c60f21fe156b9be9f8
Prikesh Savla <prikeshsavla@gmail.com>
2021-01-30 21:59:01 +0530

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module go-wall
+
+go 1.14
+
+require (
+	github.com/joho/godotenv v1.3.0
+	github.com/reujab/wallpaper v0.0.0-20201124162023-c3898ec30d2c
+)
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/reujab/wallpaper"
+)
+
+type Image struct {
+	Urls struct {
+		Full string `json:"full"`
+	} `json:"urls"`
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	// fmt.Println("Current wallpaper:", background)
+	secretKey := os.Getenv("UNSPLASH_ACCESS_KEY")
+
+	// fmt.Printf("Results: %v\n", secretKey)
+	resp, err := http.Get("https://api.unsplash.com/photos/random?client_id=" + secretKey + "&featured=true&orientation=landscape&query=landscape")
+
+	b := resp.Body
+	defer b.Close()
+
+	image := Image{}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+	err = json.Unmarshal(body, &image)
+	if err != nil {
+		panic(err.Error())
+	}
+	// fmt.Printf("Results: %v\n", image.Urls.Full)
+
+	wallpaper.SetFromURL(image.Urls.Full)
+	os.Exit(0)
+}
